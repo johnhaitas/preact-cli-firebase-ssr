925c6032e19b96329cb025132429d4a57a84f111
John Haitas <john@haitas.com>
2018-11-13 09:36:40 -0600

    webpack all of the firebase functions code

--- functions/index.js
+++
@@ -1,5 +0,0 @@
-const functions = require('firebase-functions'),
-	{ createHandler } = require('../build/ssr-build/ssr-bundle'),
-	{ template } = require('./app/build'),
-	handler = createHandler(template);
-exports.app = functions.https.onRequest(handler);
