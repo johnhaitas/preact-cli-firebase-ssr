0eb712bdc8a2a08cc9d7de07bd1df44b791ba702
John Haitas <john@haitas.com>
2018-11-28 09:00:39 -0600

    better dev build of server code

--- dev-server/webpack.preact.client.config.js
+++ dev-server/webpack.preact.client.config.js
@@ -1,27 +1,29 @@
-const { resolve } = require('path');
+const { resolve } = require('path'),
+	asyncPlugin = require('preact-cli-plugin-fast-async'),
+	WebpackConfigHelpers = require('./webpack-config-helpers');
 
 const preactCliClientConfig = require('preact-cli/lib/lib/webpack/webpack-client-config').default;
 
 const cwd = resolve(__dirname, '..'),
 	src = resolve(cwd, 'src'),
 	dest = resolve(cwd, 'build'),
+	source = dir => resolve(src, dir),
 	env = {
 		cwd,
 		isWatch: true,
 		isProd: false,
 		src,
 		dest,
-		source: dir => resolve(src, dir)
-	};
-let config = preactCliClientConfig(env);
+		source,
+		ssr: false
+	},
+	helpers = new WebpackConfigHelpers(cwd);
 
+let config = preactCliClientConfig(env);
 config.watchOptions = {
 	ignored: [
 		resolve(cwd, 'node_modules'),
-		resolve(cwd, 'build/*.js'),
-		resolve(cwd, 'build/*.css'),
-		resolve(cwd, 'build/*.map'),
-		resolve(cwd, 'build/assets'),
+		resolve(cwd, 'build'),
 		resolve(cwd, 'functions')
 	]
 };
@@ -30,5 +32,7 @@ config.entry.bundle = [
 	((config.entry.bundle instanceof Array) ? config.entry.bundle[0] : config.entry.bundle),
 	'webpack-hot-middleware/client'
 ];
+
+asyncPlugin(config);
 
 module.exports = config;
--- dev-server/webpack.preact.server.config.js
+++ dev-server/webpack.preact.server.config.js
@@ -1,18 +1,23 @@
-const { resolve } = require('path');
+const { resolve } = require('path'),
+	asyncPlugin = require('preact-cli-plugin-fast-async'),
+	WebpackConfigHelpers = require('./webpack-config-helpers');
 
 const preactCliServerConfig = require('preact-cli/lib/lib/webpack/webpack-server-config').default;
 
 const cwd = resolve(__dirname, '..'),
 	src = resolve(cwd, 'src'),
 	dest = resolve(cwd, 'build'),
+	source = dir => resolve(src, dir),
 	env = {
 		cwd,
 		isWatch: true,
-		isProd: true,
+		isProd: false,
 		src,
 		dest,
-		source: dir => resolve(src, dir)
-	};
+		source,
+		ssr: true
+	},
+	helpers = new WebpackConfigHelpers(cwd);
 
 let config = preactCliServerConfig(env);
 config.watchOptions = {
@@ -25,5 +30,11 @@ config.watchOptions = {
 
 config.entry['ssr-bundle'] = env.source('server/index.js');
 config.output.filename = '[name].js';
+
+// To fix bug in preact-cli
+helpers.getPluginsByName(config, 'ExtractTextPlugin')
+	.forEach(({ plugin }) => (plugin.options.disable = false));
+
+asyncPlugin(config);
 
 module.exports = config;
