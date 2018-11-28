00df82896e803f02b379a696761625090fadf5d6
John Haitas <john@haitas.com>
2018-11-28 09:14:34 -0600

    add unistore devtools support

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import App from './components/app';
 
 import createHistory from 'history/createBrowserHistory';
 import createStore from 'unistore';
+import devtools from 'unistore/devtools';
 
 const getProps = () => {
 		const history = createHistory(),
-			store = createStore(window.__STORE_STATE || {});
+			initialState = window.__STORE_STATE || {},
+			store = process.env.NODE_ENV === 'production' ?  createStore(initialState) : devtools(createStore(initialState));
 		// other things you might do here ...
 		// * initialize something via a client-side configuration on the `window` object
 		return { history, store };
