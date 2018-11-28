80813930ed46297efda700ac67a9eb2b753ace5f
John Haitas <john@haitas.com>
2018-11-28 12:27:43 -0600

    only recreate handler after each compilation is done rather than on each request

--- dev-server/index.js
+++ dev-server/index.js
@@ -3,7 +3,8 @@ let serverConfig = require('./webpack.preact.server.config'),
 serverConfig.name = 'server';
 clientConfig.name = 'client';
 
-const webpack = require('webpack'),
+const requireFromString = require('require-from-string'),
+	webpack = require('webpack'),
 	webpackConfig = [ serverConfig, clientConfig ],
 	compiler = webpack(webpackConfig),
 	serverCompiler = compiler.compilers.find(compiler => compiler.name === 'server'),
@@ -13,14 +14,16 @@ const webpack = require('webpack'),
 	express = require('express'),
 	app = express();
 
+let handler;
+compiler.plugin('done', stats => {
+	const template = clientCompiler.outputFileSystem.readFileSync(`${clientConfig.output.path}/index.html`).toString('utf-8'),
+		{ createHandler } = requireFromString(serverCompiler.outputFileSystem.readFileSync(`${serverConfig.output.path}/ssr-bundle.js`).toString('utf-8'));
+	handler = createHandler(template);
+});
+
 app.use(webpackIsomorphicDevMiddleware);
 app.use(webpackHotMiddleware);
-app.use((req, res, next) => {
-	const template = clientCompiler.outputFileSystem.readFileSync(`${clientConfig.output.path}/index.html`).toString('utf-8'),
-		{ createHandler } = res.locals.isomorphic.exports,
-		handler = createHandler(template);
-	handler(req, res);
-});
+app.use((req, res, next) => handler(req, res));
 
 app.listen(8080, () => console.log('app is listening!'));
 
